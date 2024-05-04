546606c3c4a7eaa292ff6f27a50666b98b055a6d
Evan Graham <me@luakt.net>
2024-05-04 02:40:08 +0100

    to der

--- src/format/der.rs
+++ src/format/der.rs
@@ -7,7 +7,7 @@ use openssl::{
 
 pub fn to_der_pubkey(pubkey: &PublicKey) -> OsshResult<Vec<u8>> {
     let der = match &pubkey.key {
-        PublicKeyType::RSA(key) => key.ossl_rsa().public_key_to_der_pkcs1()?,
+        PublicKeyType::RSA(key) => key.ossl_rsa().public_key_to_der()?,
         PublicKeyType::DSA(key) => key.ossl_pkey()?.public_key_to_der()?,
         PublicKeyType::ECDSA(key) => key.ossl_pkey()?.public_key_to_der()?,
         PublicKeyType::ED25519(key) => key.ossl_pkey()?.public_key_to_der()?,
